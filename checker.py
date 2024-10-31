a3738908d6a1061400c6a22a9cf7220aaa987b97
shre-555 <shreyakundu6782@gmail.com>
2024-10-31 23:28:12 +0530

    "fixed let const"

--- checker.py
+++ checker.py
@@ -2,53 +2,56 @@ import ply.lex as lex
 
 # List of token names.   This is always required
 tokens = (
-   'NUMBER',
-   'SELECTION_STATEMENTS',
-   'VARIABLE_DECLARATION_STATEMENT', #var,let,const
-   'VARIABLE_NAME',
-   'VARIABLE_TYPE',
-   'EQUAL',
-   'TYPE_DECLARATOR',
-   'PUNCTUATOR',
-   'LSQUARE',
-   'RSQUARE',
-   'KEYWORDS'
+    'NUMBER',
+    'SELECTION_STATEMENTS',
+    'VARIABLE_DECLARATION_STATEMENT', # var, let, const
+    'VARIABLE_TYPE',
+    'EQUAL',
+    'TYPE_DECLARATOR',
+    'PUNCTUATOR',
+    'LSQUARE',
+    'RSQUARE',
+    'KEYWORDS',
+    'VARIABLE_NAME',
+    'STRING'
 )
 
-# Regular expression rules for simple tokens
-t_EQUAL   = r'='
-t_LSQUARE  = r'\['
-t_RSQUARE  = r'\]'
-t_TYPE_DECLARATOR= r':'
-t_PUNCTUATOR= r'(;|")'
-t_SELECTION_STATEMENTS = r'(else if | if | else)'
-t_VARIABLE_DECLARATION_STATEMENT=r'(let | const)'
-t_VARIABLE_NAME= r'(([A-Za-z_]+[0-9]*)+)'
-t_VARIABLE_TYPE=r'(number|string|boolean|object)'
-# t_KEYWORDS= 
-
-# A regular expression rule with some action code
+# Regular expression rules for tokens
 def t_NUMBER(t):
     r'\d+'
     t.value = int(t.value)    
     return t
 
-# Define a rule so we can track line numbers
+def t_STRING(t):
+    r'\"([^\\\"]|\\.)*\"' #quotation marks should have starting and ending
+    return t
+
+t_SELECTION_STATEMENTS = r'\b(else if|if|else)\b'
+t_VARIABLE_TYPE = r'\b(number|string|boolean|object)\b'
+t_VARIABLE_DECLARATION_STATEMENT = r'\b(let|const)\b'
+t_VARIABLE_NAME = r'(?!(if|else|let|const|number|string|boolean|object)\b)[a-zA-Z_][a-zA-Z0-9_]*'
+t_PUNCTUATOR = r';'
+t_EQUAL = r'='
+t_LSQUARE = r'\['
+t_RSQUARE = r'\]'
+t_TYPE_DECLARATOR = r':'
+t_KEYWORDS = r'\b(return|function|for|while|do|break|continue)\b'
+
+# Track line numbers
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
 
-# A string containing ignored characters (spaces and tabs)
-t_ignore  = ' \t'
+# Ignored characters (spaces and tabs)
+t_ignore = ' \t'
 
 # Error handling rule
 def t_error(t):
-    print("Illegal character '%s'" % t.value[0])
+    print(f"Illegal character '{t.value[0]}' at line {t.lexer.lineno}")
     t.lexer.skip(1)
 
 # Build the lexer
 lexer = lex.lex()
-
 
 # Test it out
 data = '''
